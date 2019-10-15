69286cfb0305bc8e0d6240363b370d50a2c7658a
Marcos <marcosapmf22@gmail.com>
2019-10-14 22:54:43 -0300

    testing category domain

--- src/main/java/com/marcos/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/marcos/cursomc/resources/CategoriaResource.java
@@ -1,15 +1,26 @@
 package com.marcos.cursomc.resources;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.marcos.cursomc.domain.Categoria;
 
 @RestController
 @RequestMapping(value = "/categorias")
 public class CategoriaResource {
 
 	@GetMapping
-	public String listar() {
-		return "REST está funcionando";
+	public List<Categoria> listar() {
+		
+		Categoria cat1 = new Categoria(1, "informática");
+		Categoria cat2 = new Categoria(2, "escritório");
+		
+		List<Categoria> catList = Arrays.asList(cat1, cat2);
+		
+		return catList;
 	}
 }
