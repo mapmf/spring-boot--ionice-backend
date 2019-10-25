7edf1219e05ec86ad136903b3ece6924a65bb212
Marcos <marcosapmf22@gmail.com>
2019-10-24 22:03:33 -0300

    paginated categoria endpoint

--- src/main/java/com/marcos/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/marcos/cursomc/resources/CategoriaResource.java
@@ -5,6 +5,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
@@ -76,5 +78,19 @@ public class CategoriaResource {
 															.collect(Collectors.toList());
 		
 		return ResponseEntity.ok(categoriaDTOList);
+	}
+	
+	@GetMapping("/page")
+	public ResponseEntity<Page<CategoriaDTO>> findPage(
+			@RequestParam(name = "page", defaultValue = "0") Integer page, 
+			@RequestParam(name = "linesPerPage", defaultValue = "24")  Integer linesPerPage, 
+			@RequestParam(name = "ordeBy", defaultValue = "nome")  String orderBy, 
+			@RequestParam(name = "direction", defaultValue = "ASC")  String direction) {
+		
+		Page<Categoria> categoriaList = service.findPage(page, linesPerPage, orderBy, direction);
+		
+		Page<CategoriaDTO> categoriaDTOList = categoriaList.map(c -> new CategoriaDTO(c));
+		
+		return ResponseEntity.ok(categoriaDTOList); 
 	}
 }
