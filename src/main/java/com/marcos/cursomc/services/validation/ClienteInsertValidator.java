0518d3f37da5bcdf1fb294cdd79ffed60b2df16a
Marcos <marcosapmf22@gmail.com>
2019-10-29 22:42:02 -0300

    custom email validation

--- src/main/java/com/marcos/cursomc/services/validation/ClienteInsertValidator.java
+++ src/main/java/com/marcos/cursomc/services/validation/ClienteInsertValidator.java
@@ -6,28 +6,41 @@ import java.util.List;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.marcos.cursomc.domain.Cliente;
 import com.marcos.cursomc.domain.enums.TipoCliente;
 import com.marcos.cursomc.dto.ClienteNewDTO;
+import com.marcos.cursomc.repositories.ClienteRepository;
 import com.marcos.cursomc.resources.exception.handler.FieldMessage;
 import com.marcos.cursomc.services.validation.utils.BR;
 
 public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO> {
 
+	@Autowired
+	private ClienteRepository clienteRepo;
+	
 	@Override
 	public void initialize(ClienteInsert constraintAnnotation) {
 	}
 
 	@Override
-	public boolean isValid(ClienteNewDTO value, ConstraintValidatorContext context) {
+	public boolean isValid(ClienteNewDTO objDto, ConstraintValidatorContext context) {
 
 		List<FieldMessage> list = new ArrayList<FieldMessage>();
 
-		if(value.getTipo().equals(TipoCliente.PESSOAFISICA.getId()) && !BR.isValidCpf(value.getCpfOuCnpj())) {
+		if(objDto.getTipo().equals(TipoCliente.PESSOAFISICA.getId()) && !BR.isValidCpf(objDto.getCpfOuCnpj())) {
 			list.add(new FieldMessage("cpfOuCnpj", "CPF inválido"));
 		}
 		
-		if(value.getTipo().equals(TipoCliente.PESSOAJURIDICA.getId()) && !BR.isValidCnpj(value.getCpfOuCnpj())) {
+		if(objDto.getTipo().equals(TipoCliente.PESSOAJURIDICA.getId()) && !BR.isValidCnpj(objDto.getCpfOuCnpj())) {
 			list.add(new FieldMessage("cpfOuCnpj", "CNPJ inválido"));
+		}
+		
+		Cliente cliente = clienteRepo.findByEmail(objDto.getEmail());
+		
+		if(cliente != null) {
+			list.add(new FieldMessage("email", "Email já existente"));
 		}
 		
 		for (FieldMessage e : list) {
