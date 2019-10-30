b77509484a5158c23fec0d69762bbd58be7e5a9a
Marcos <marcosapmf22@gmail.com>
2019-10-29 21:52:13 -0300

    adjusting cliente delete

--- src/main/java/com/marcos/cursomc/services/ClienteService.java
+++ src/main/java/com/marcos/cursomc/services/ClienteService.java
@@ -69,7 +69,7 @@ public class ClienteService {
 			
 		} catch(DataIntegrityViolationException e) {
 			
-			throw new DataIntegrityException("Não é possível excluir uma cliente porque há entidades relacionadas");
+			throw new DataIntegrityException("Não é possível excluir uma cliente porque há pedidos relacionados");
 		}
 	}
 
