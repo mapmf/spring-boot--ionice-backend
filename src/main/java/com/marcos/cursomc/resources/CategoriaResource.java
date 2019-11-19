d24ae488f43e922e7c64fc8b6c69df1b1ab56221
Marcos <marcosapmf22@gmail.com>
2019-11-18 21:49:29 -0300

    specific profile endpoint authorizations

--- src/main/java/com/marcos/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/marcos/cursomc/resources/CategoriaResource.java
@@ -9,6 +9,7 @@ import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -39,6 +40,7 @@ public class CategoriaResource {
 		return ResponseEntity.ok(categoria);
 	}
 	
+	@PreAuthorize("hasAnyRole('ADMIN')")
 	@PostMapping
 	public ResponseEntity<Void> insert(@Valid @RequestBody CategoriaDTO objDTO){
 		
@@ -54,6 +56,7 @@ public class CategoriaResource {
 		return ResponseEntity.created(uri).build();
 	}
 	
+	@PreAuthorize("hasAnyRole('ADMIN')")
 	@PutMapping("/{id}")
 	public ResponseEntity<Void> update(@PathVariable("id") Integer id, @Valid @RequestBody CategoriaDTO objDTO) {
 		
@@ -66,6 +69,7 @@ public class CategoriaResource {
 		return ResponseEntity.noContent().build();
 	}
 	
+	@PreAuthorize("hasAnyRole('ADMIN')")
 	@DeleteMapping("/{id}")
 	public ResponseEntity<Categoria> delete(@PathVariable("id") Integer id) {
 		
--- src/main/java/com/marcos/cursomc/resources/ClienteResource.java
+++ src/main/java/com/marcos/cursomc/resources/ClienteResource.java
@@ -9,6 +9,7 @@ import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -67,6 +68,7 @@ public class ClienteResource {
 		return ResponseEntity.noContent().build();
 	}
 	
+	@PreAuthorize("hasAnyRole('ADMIN')")
 	@DeleteMapping("/{id}")
 	public ResponseEntity<Cliente> delete(@PathVariable("id") Integer id) {
 		
@@ -75,18 +77,20 @@ public class ClienteResource {
 		return ResponseEntity.noContent().build();
 	}
 	
+	@PreAuthorize("hasAnyRole('ADMIN')")
 	@GetMapping
 	public ResponseEntity<List<ClienteDTO>> findAll() {
 		
-		List<Cliente> categoriaList = service.findAll();
+		List<Cliente> clienteList = service.findAll();
 		
-		List<ClienteDTO> categoriaDTOList = categoriaList.stream()
+		List<ClienteDTO> clienteDTOList = clienteList.stream()
 															.map(c -> new ClienteDTO(c))
 															.collect(Collectors.toList());
 		
-		return ResponseEntity.ok(categoriaDTOList);
+		return ResponseEntity.ok(clienteDTOList);
 	}
 	
+	@PreAuthorize("hasAnyRole('ADMIN')")
 	@GetMapping("/page")
 	public ResponseEntity<Page<ClienteDTO>> findPage(
 			@RequestParam(name = "page", defaultValue = "0") Integer page, 
@@ -94,11 +98,11 @@ public class ClienteResource {
 			@RequestParam(name = "ordeBy", defaultValue = "nome")  String orderBy, 
 			@RequestParam(name = "direction", defaultValue = "ASC")  String direction) {
 		
-		Page<Cliente> categoriaList = service.findPage(page, linesPerPage, orderBy, direction);
+		Page<Cliente> clienteList = service.findPage(page, linesPerPage, orderBy, direction);
 		
-		Page<ClienteDTO> categoriaDTOList = categoriaList.map(c -> new ClienteDTO(c));
+		Page<ClienteDTO> clienteDTOList = clienteList.map(c -> new ClienteDTO(c));
 		
-		return ResponseEntity.ok(categoriaDTOList); 
+		return ResponseEntity.ok(clienteDTOList); 
 	}
 
 }
