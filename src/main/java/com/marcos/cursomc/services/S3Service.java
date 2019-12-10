6e4d4884c6af17a51e866b6eda066eae054129bc
Marcos <marcosapmf22@gmail.com>
2019-12-10 20:31:01 -0300

    deal with amazon exceptions

--- src/main/java/com/marcos/cursomc/services/S3Service.java
+++ src/main/java/com/marcos/cursomc/services/S3Service.java
@@ -14,6 +14,7 @@ import org.springframework.web.multipart.MultipartFile;
 
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.model.ObjectMetadata;
+import com.marcos.cursomc.services.exceptions.FileException;
 
 @Service
 public class S3Service {
@@ -37,7 +38,7 @@ public class S3Service {
 			return uploadFile(is, filename, contentType);
 			
 		} catch (IOException e) {
-			throw new RuntimeException("Erro de I/O: " + e.getMessage());
+			throw new FileException("Erro de I/O: " + e.getMessage());
 		}
 	}
 
@@ -57,7 +58,7 @@ public class S3Service {
 			return s3Client.getUrl(bucketName, filename).toURI();
 
 		} catch (URISyntaxException e) {
-			throw new RuntimeException("Erro ao converter URL para URI");
+			throw new FileException("Erro ao converter URL para URI");
 		}
 	}
 }
