02c55eebcc26164288471415a996902f4076318a
Marcos <marcosapmf22@gmail.com>
2019-12-16 21:01:08 -0300

    adding authentication cors header

--- src/main/java/com/marcos/cursomc/security/JWTAuthenticationFilter.java
+++ src/main/java/com/marcos/cursomc/security/JWTAuthenticationFilter.java
@@ -57,6 +57,8 @@ public class JWTAuthenticationFilter extends UsernamePasswordAuthenticationFilte
 		String token = jwtUtil.generateToken(username);
 		
 		response.addHeader("Authorization", "Bearer " + token);
+		
+		response.addHeader("access-control-expose-headers", "Authorization");
 	
 	}
 }
